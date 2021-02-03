d50bfced6aa72046d99c8b9a33bdb61aa555f90c
Kevin Y. Kim <kevinyckim33@gmail.com>
2021-02-03 14:01:47 -0500

    Finish 8 of 279: Catching Errors with Typescript

--- index.ts
+++ index.ts
@@ -2,20 +2,39 @@ import axios from "axios";
 
 const url = "http://jsonplaceholder.typicode.com/todos/1";
 
-axios.get(url).then((response) => {
-  console.log(response.data);
-  const todo = response.data;
+// interface: defines structure of an object
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+axios.get(url).then((response) => {
+  // Response.data has properties of:
+  // id
+  // title
+  // completed
+  const todo = response.data as Todo; // cool clutch line - very applicable example Stephen!
+
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
 
   console.log(`
-    The Todo with ID: ${ID}
+    The Todo with ID: ${id}
     Has a title of: ${title}
     Is it finished? ${finished}
   `);
 });
+
+/*
+{
+"userId": 1,
+"id": 1,
+"title": "delectus aut autem",
+"completed": false
+}
+*/
 
 // run tsc index.ts in Terminal to get it converted to index.js
 
